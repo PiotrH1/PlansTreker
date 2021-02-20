dd36f1915ebb412831478f7bdfd7daca89213925
PiotrH1 <ph.netdevelop@gmail.com>
2021-02-20 12:36:53 -0500

    add files

---
+++ PlansTrekerSolution/PlansTreker/Data/PlansTrekerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PlansTreker.Models;
+
+namespace PlansTreker.Data
+{
+    public class PlansTrekerContext : DbContext
+    {
+        public PlansTrekerContext (DbContextOptions<PlansTrekerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PlansTreker.Models.Destination> Destination { get; set; }
+    }
+}
---
+++ PlansTrekerSolution/PlansTreker/Models/Destination.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlansTreker.Models
+{
+    public class Destination
+    {
+        public int id { get; set; }
+        public string destination { get; set; }
+        public string destinationDescription { get; set; }
+        
+        public Destination()
+        {
+
+        }
+    }
+}
