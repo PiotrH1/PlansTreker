21b70c22de9240fd067088de1ad94747e4f9404c
PiotrH1 <ph.netdevelop@gmail.com>
2021-02-21 00:11:31 -0500

    process

---
+++ PlanTracker/PlanTracker.Web/Models/DirectionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using PlanTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlanTracker.Web.Models
+{
+    public class DirectionViewModel
+    {
+        public Direction Direction { get; set; }
+        public List<Todo> Todoes { get; set; }
+    }
+}
